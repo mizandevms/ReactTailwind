30a6ac4b78085a27f50140c1350979051f3d7d48
Mizanur Rahman Mizan <mizanurrahmanmizan@pop-os.localdomain>
2021-12-31 01:14:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import "./App.css"
+import Home from "./FrontEnd/Components/Home/Home"
+import Footer from "./FrontEnd/Partials/Footer/Footer"
+import Header from "./FrontEnd/Partials/Header/Header"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+			<div className='App'>
+				<Routes>
+					<Route path='/' element={<Home />} />
+				</Routes>
+			</div>
+			<Footer />
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
