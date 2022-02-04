0742ddcc4bda87b778ac914704d2be0fb0c0a26f
Mizanur Rahman Mizan <mizanurrahmanmizan@pop-os.localdomain>
2022-02-04 11:44:03 +0600

    gsap test

--- src/FrontEnd/Components/Animation/Animation.jsx
+++ src/FrontEnd/Components/Animation/Animation.jsx
@@ -1,11 +1,27 @@
-import React from "react"
+import {gsap} from "gsap"
+import React, {useEffect, useRef} from "react"
 
 export default function Animation() {
+	const boxRef = useRef()
+
+	useEffect(() => {
+		gsap.fromTo(
+			boxRef.current,
+			{
+				x: 100,
+				opacity: 0,
+			},
+			{
+				x: 0,
+				opacity: 1,
+			}
+		)
+	})
 	return (
 		<div className='max-w-4xl mx-auto py-12 h-[50rem]'>
-			<p p className='text-center text-3xl font-extrabold text-cyan-700 space-y-3 flex flex-col'>
-				<span>Hello Listener,</span>
-				<span>Welcome Here</span>
+			<p p className='text-center text-3xl font-extrabold text-cyan-700 space-y-4 flex flex-col ' ref={boxRef}>
+				<span className='border border-gray-200 shadow-md p-4 rounded-md'>Hello Listener,</span>
+				<span className='border border-gray-200 shadow-md p-4 rounded-md'>Welcome Here</span>
 			</p>
 		</div>
 	)
